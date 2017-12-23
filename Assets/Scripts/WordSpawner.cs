4850e0879d3ee274380921093088b7dff68b16f0
Ken De Pauw <Morinethar5@users.noreply.github.com>
2017-12-23 18:36:06 +0000

    V1 of Falling Words

---
+++ Assets/Scripts/WordSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSpawner : MonoBehaviour {
+
+    public GameObject wordPrefab;
+    public Transform wordCanvas;
+
+    public WordDisplay SpawnWord() 
+    {
+        Vector3 randomP = new Vector3(Random.Range(-280f, 280f), 7f);
+
+        GameObject wordObject = Instantiate(wordPrefab, randomP, Quaternion.identity, wordCanvas);
+        WordDisplay wordDisp = wordObject.GetComponent<WordDisplay>();
+
+        return wordDisp;
+    }
+}
